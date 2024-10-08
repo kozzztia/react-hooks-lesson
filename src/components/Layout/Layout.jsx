a802424e88bd202c14915a1a60a8d6c6940e574c
kozzztia <kozzztia@gmail.com>
2024-10-08 20:58:56 +0300

    navigation icons header

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,14 +1,19 @@
 import { PropTypes } from 'prop-types';
 import styles from './styles.module.css';
 import Navigation from '../Navigation/Navigation';
+import photo from '../../assets/photos/profile-photo.png';
+import Icon from '../icon/Icon';
 
 const Layout = ({ children }) => {
     return (
         <main className={styles.main}>
             <header className={styles.header}>
-                header
+                <button>
+                    <Icon name="message"/>
+                </button>
+                <img src={photo} alt="photo" />
             </header>
-            <Navigation className={styles.nav}/>
+            <Navigation className={styles.nav} />
             <section className={styles.section}>
                 {children}
             </section>
--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -4,20 +4,29 @@ import styles from './styles.module.css';
 import { NavLink } from 'react-router-dom';
 import { navLinks } from '../../consts';
 
+import photo from '../../assets/photos/profile-photo.png';
+import Icon from '../icon/Icon';
+
 
 const Navigation = ({ className }) => {
 
     return (
         <nav className={`${styles.navigation} ${className}`}>
+            <figure>
+                <img src={photo} alt="photo" />
+                <figcaption>
+                    scott grand
+                </figcaption>    
+            </figure>
             <ul>
                 {
                     navLinks.map((link) => (
                         <li key={link.id} className={styles.navLink}>
                             <NavLink
                                 to={link.path}
-                                className={({ isActive }) => isActive ? styles.active : ""}
-                            >
-                                {link.name}
+                                className={({ isActive }) => isActive ? styles.active : ""}>
+                                <Icon name={link.name} />
+                                <span>{link.name}</span>
                             </NavLink>
                         </li>
                     ))
